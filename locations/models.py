92a0c63c4d3002248b11e28173c645f9bcb26e86
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-04-04 23:43:08 +0200

    Add country & city models

--- locations/models.py
+++ locations/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=255, verbose_name=_('Country Name'))
+    code = models.CharField(max_length=10, null=True, blank=True, verbose_name=_('Country Code'))
+    create_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Create At'))
+    update_at = models.DateTimeField(auto_now=True, verbose_name=_('Update At'))
+
+    class Meta:
+        verbose_name = _('Country')
+        verbose_name_plural = _('Countries')
+        ordering = ('-create_at', '-update_at')
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    country = models.ForeignKey(
+        Country,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='cities',
+        verbose_name=_('Country')
+    )
+    name = models.CharField(max_length=255, verbose_name=_('City Name'))
+    code = models.CharField(max_length=10, null=True, blank=True, verbose_name=_('City Code'))
+    create_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Create At'))
+    update_at = models.DateTimeField(auto_now=True, verbose_name=_('Update At'))
+
+    class Meta:
+        db_table = _('City')
+        verbose_name_plural = _('Cities')
+        ordering = ('-create_at', '-update_at')
+
+    def __str__(self):
+        return self.name
