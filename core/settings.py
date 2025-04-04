4ac8988e7281a2fb5b2840ee7e7907f8b0fb8e58
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-04-05 00:49:18 +0200

    Add more third party apps

--- core/settings.py
+++ core/settings.py
@@ -58,6 +58,8 @@ ALLOWED_HOSTS = [
 
 INSTALLED_APPS = [
     'jazzmin',
+    'modeltranslation',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
