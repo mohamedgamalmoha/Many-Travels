cbd00c205921790875defc63d61c755722195376
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-04-04 22:38:47 +0200

    Write user serializer

---
+++ accounts/api/serializers.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.backends import get_user_model
+
+from rest_framework import serializers
+
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        exclude = ('password', 'is_superuser', 'is_staff', 'groups', 'user_permissions')
