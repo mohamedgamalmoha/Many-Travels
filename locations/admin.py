36ec4d79469a10f76d87cb720871cb2f9ca9fa31
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-04-04 23:44:54 +0200

    Write admin page for country & city

--- locations/admin.py
+++ locations/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from locations.models import Country, City
+
+
+class CityInline(admin.TabularInline):
+    model = City
+    extra = 1
+    readonly_fields = ('create_at', 'update_at')
+
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'country', 'create_at', 'update_at')
+    fieldsets = (
+        (_('Main Info'), {'fields': ('country', 'name', 'code')}),
+        (_('Important Dates'), {'fields': ('create_at', 'update_at')})
+    )
+    search_fields = ('name', 'code')
+    list_filter = ('country', 'create_at', 'update_at')
+    ordering = ('-create_at', '-update_at')
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('country')
+
+
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'create_at', 'update_at')
+    fieldsets = (
+        (_('Main Info'), {'fields': ('name', 'code')}),
+        (_('Important Dates'), {'fields': ('create_at', 'update_at')})
+    )
+    search_fields = ('name', 'code')
+    list_filter = ('create_at', 'update_at')
+    ordering = ('-create_at', '-update_at')
+    inlines = [CityInline]
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('cities')
+
+
+admin.site.register(City, CityAdmin)
+admin.site.register(Country, CountryAdmin)
