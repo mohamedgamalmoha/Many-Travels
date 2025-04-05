928f5bf4eff98ad2da6cf981ba5e05cdfc4aceb0
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-04-05 19:17:00 +0200

    Add state admin pages

--- locations/admin.py
+++ locations/admin.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 from modeltranslation.admin import TranslationAdmin, TranslationInlineModelAdmin
 
-from locations.models import Country, City
+from locations.models import Country, City, State
 
 
 class CityInline(TranslationInlineModelAdmin):
@@ -12,18 +12,10 @@ class CityInline(TranslationInlineModelAdmin):
     readonly_fields = ('create_at', 'update_at')
 
 
-class CityAdmin(TranslationAdmin):
-    list_display = ('name', 'code', 'country', 'create_at', 'update_at')
-    fieldsets = (
-        (_('Main Info'), {'fields': ('country', 'name', 'code')}),
-        (_('Important Dates'), {'fields': ('create_at', 'update_at')})
-    )
-    search_fields = ('name', 'code')
-    list_filter = ('country', 'create_at', 'update_at')
-    ordering = ('-create_at', '-update_at')
-
-    def get_queryset(self, request):
-        return super().get_queryset(request).select_related('country')
+class StateInline(TranslationInlineModelAdmin):
+    model = State
+    extra = 1
+    readonly_fields = ('create_at', 'update_at')
 
 
 class CountryAdmin(TranslationAdmin):
@@ -41,5 +33,35 @@ class CountryAdmin(TranslationAdmin):
         return super().get_queryset(request).prefetch_related('cities')
 
 
+class CityAdmin(TranslationAdmin):
+    list_display = ('name', 'code', 'country', 'create_at', 'update_at')
+    fieldsets = (
+        (_('Main Info'), {'fields': ('country', 'name', 'code')}),
+        (_('Important Dates'), {'fields': ('create_at', 'update_at')})
+    )
+    search_fields = ('name', 'code')
+    list_filter = ('country', 'create_at', 'update_at')
+    ordering = ('-create_at', '-update_at')
+    inlines = [StateInline]
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('country')
+
+
+class StateAdmin(TranslationAdmin):
+    list_display = ('name', 'code', 'city', 'create_at', 'update_at')
+    fieldsets = (
+        (_('Main Info'), {'fields': ('city', 'name', 'code')}),
+        (_('Important Dates'), {'fields': ('create_at', 'update_at')})
+    )
+    search_fields = ('name', 'code')
+    list_filter = ('create_at', 'update_at')
+    ordering = ('-create_at', '-update_at')
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('city')
+
+
+admin.site.register(State, StateAdmin)
 admin.site.register(City, CityAdmin)
 admin.site.register(Country, CountryAdmin)
