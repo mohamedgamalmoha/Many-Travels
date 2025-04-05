746274e162ec33c6d083150788167e44c74655f7
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-04-05 19:10:28 +0200

    Add state model

--- locations/models.py
+++ locations/models.py
@@ -37,3 +37,25 @@ class City(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class State(models.Model):
+    city = models.ForeignKey(
+        City,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='states',
+        verbose_name=_('City')
+    )
+    name = models.CharField(max_length=255, verbose_name=_('State Name'))
+    code = models.CharField(max_length=10, null=True, blank=True, verbose_name=_('State Code'))
+    create_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Create At'))
+    update_at = models.DateTimeField(auto_now=True, verbose_name=_('Update At'))
+
+    class Meta:
+        db_table = _('State')
+        verbose_name_plural = _('States')
+        ordering = ('-create_at', '-update_at')
+
+    def __str__(self):
+        return self.name
