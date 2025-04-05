8a8505d8c455800365914fa4e3eec4e992807485
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-04-05 19:23:46 +0200

    Write days of week enum

--- agency/enums.py
+++ agency/enums.py
@@ -11,3 +11,13 @@ class SocialMediaPlatform(models.TextChoices):
     PINTEREST = 'pinterest', _("Pinterest")
     PHONE_NUMBER_1 = 'phone_number_1', _('Phone Number 1')
     PHONE_NUMBER_2 = 'phone_number_2', _('Phone Number 2')
+
+
+class DaysOfWeekChoice(models.TextChoices):
+    SATURDAY = 'SAT', _("Saturday")
+    SUNDAY = 'SUN', _("Sunday")
+    MONDAY = 'MON', _("Monday")
+    TUESDAY = 'TUE', _("Tuesday")
+    WEDNESDAY = 'WED', _("Wednesday")
+    THURSDAY = 'THU', _("Thursday")
+    FRIDAY = 'FRI', _("Friday")
