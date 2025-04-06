bfb58fd9b14a901ee83fc19bb48855f3df726dd3
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-04-06 11:40:30 +0200

    Add app verbose name

--- agency/apps.py
+++ agency/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class AgencyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'agency'
+    verbose_name = _('Agencies')
